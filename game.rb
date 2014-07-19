2482c173a20d2af849a130f67763bf3892971254
Sai To Yeung <saitoyeung@gmail.com>
2014-07-19 14:29:13 -0400

    Implements initialize method in Game class

--- game.rb
+++ game.rb
@@ -1,6 +1,26 @@
+require_relative 'player'
+
 class Game
 
 	def initialize
-		
+		@players = []
+		4.times do |num|
+			@players << Player.new("Player #{num}")
+		end
+
+		@stock_pile = Deck.new.shuffle!
+		@discard_pile = []
+
+		5.times do |num|
+			@players.each do |player|
+				player << @stock_pile.pop
+			end
+		end
+
+		@discard_pile << @stock_pile.pop
+
 	end
+
+	
+
 end
